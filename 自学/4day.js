c27b776cf9cb29a3ad6f787d43deb2e0e52e7a6b
xzc <xzc990310@gmail.com>
2022-03-05 04:55:38 -0500

    4day

--- 自学/4day.js
+++ 自学/4day.js
@@ -21,3 +21,45 @@ function fanzhuan(x) {
     return ge + "" + shi / 10 + "" + bai / 100 + ""
   }
 }
+
+//斐波那契数列第n项
+function fbnq(n) {
+  let a = 1
+  let b = 1
+  let c = 0
+
+  for (let i = 0; i < n - 1; i++) {
+    c = a + b
+    a = b
+    b = c
+  } return a
+}
+
+//判断数字有几位  
+debugger;
+function pd(x) {//123
+  位数计次 = 0
+
+  while (x != 0) {
+    x = x - (x % 10)
+    x = x / 10
+    位数计次++
+  } return (位数计次)
+}
+//判断过水仙花数后，判断是否为水仙花数
+function sxh(x) {
+  let sum = 0
+  let ge = 0
+  let a = x
+
+  for (let i = 0; i < pd(x); i++) {
+    ge = a % 10
+    sum += ge ** pd(x)
+    a = (a - ge) / 10
+  } if (sum == x) {
+    return true
+  } else {
+    return false
+  }
+}
+
