c4f7a350e325a4209599171aeba0fd44eba0780f
xzc <xzc990310@gmail.com>
2022-03-07 07:32:50 -0500

    6day

--- 自学/6day.js
+++ 自学/6day.js
@@ -1,4 +1,5 @@
 //某一年的第一月的第一天是星期几
+//年份和月份是周期性的变化，得知从第一个世纪到现在月份的总计天数，就可以推导出这个月的第一天是星期几
 
 //先去分是否为闰年
 function 闰年(year) {
@@ -6,6 +7,11 @@ function 闰年(year) {
     return true
   } return false
 }
+//润年的判断条件为能被4整除，但不能被100整除，但又能被400整除的数字，此处输入的形参，进入判断后输出真或假，可优化为
+
+// function 闰年(year) {
+//   return year % 4 == 0 && year % 100 != 0 || year % 400 == 0 
+// }
 
 //再计算每个月有多少天
 function 月(year, month) {
@@ -32,11 +38,12 @@ function 月(year, month) {
   }
 }
 
+//用switch语法处理12个月的不同情况，区分大小月和闰年与否的2月特殊情况，返回对应的天数，此处可以使用多个if语句进行判断
 
 function rq(year, month) {
   let day = 0
-  let monthDay = 0
-  let allDay = 0
+
+  //先给一个记录参数，记录天数
 
   for (let i = 1; i < year; i++) {
     if (闰年(i)) {
@@ -44,11 +51,19 @@ function rq(year, month) {
     } else day += 365
   }
 
+  //计算年数共计的天数，循环不同的情况并记录，此处有优化写法，
+  //var = year - 1
+  //(形参提供的年份还没有完全结束，要计算共计的天数需要分开计算年和月的值)
+  //var day = y * 365 + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(y / 400)
+  //直接在这里区分，闰年非闰年的天数，运算后直接出结果，可以省去一个函数，得出年份的总天数
+
+
   for (let j = 1; j < month; j++) {
     day += 月(year, j)
-  } day = (day + 1 ) % 7
-return day
+  }//循环12个月的不同月份，带入区分每个月有多少天的函数，加上年份的总天数，得到年加月的总计天数
+  day = (day + 1) % 7
+  //一周共计7天，对照实际的星期数，修改结果
+  return day
 }
-
 
 
