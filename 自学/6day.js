71f66b90b29208edc188297f5b0932d5869895f8
xzc <xzc990310@gmail.com>
2022-03-07 04:23:14 -0500

    6day

--- 自学/6day.js
+++ 自学/6day.js
@@ -7,37 +7,48 @@ function 闰年(year) {
   } return false
 }
 
-//再去区分月份所占了本年的多少天
+//再计算每个月有多少天
 function 月(year, month) {
-  let day = 0
-
-  for (let i = 1; i <= month; i++) {
-      if (i == 2) {
-        day = 29
-      }if (!闰年(year)) {
-        if (i == 2) {
-          day = 28
-        }
-      }if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) {
-        day = 31
-      } if (i == 4 || i == 6 || i == 9 || i == 11)
-        day = 30
-    } return day
-  }
-
-
-  function rq(year, month) {
-    let nday = 0
-    let yday = 0
-    let jz = (2022, 8)//周一
-
-    for (let i = 1; i <= year; i++) {
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31;
+    case 2:
       if (闰年(year)) {
-        nday += 366
-      } else nday += 365
-    }
-
-
+        return 29;
+      } else {
+        return 28;
+      }
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
   }
+}
+
+
+function rq(year, month) {
+  let day = 0
+  let monthDay = 0
+  let allDay = 0
+
+  for (let i = 1; i < year; i++) {
+    if (闰年(i)) {
+      day += 366
+    } else day += 365
+  }
+
+  for (let j = 1; j < month; j++) {
+    day += 月(year, j)
+  } day = (day + 1 ) % 7
+return day
+}
+
 
 
