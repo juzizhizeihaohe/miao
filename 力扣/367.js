e7d680d22669cef199d8b7eb76913120030684c4
xzc <xzc990310@gmail.com>
2022-03-08 09:07:07 -0500

    7day

--- 力扣/367.js
+++ 力扣/367.js
@@ -4,16 +4,26 @@
  * 根据完全平方数的性质，我们只需要直接判断 num 的平方根 是否为整数即可。
  */
 
- 遍历
 var isPerfectSquare = function (num) {
-  let x = 1
-  let y = 1
-  while (y <= num) {
-    if (y == num) {
+  let j = 1
+  let sum = 0
+
+  for (let i = 1; sum <= num; i++, j++) {
+    sum = i * j
+    if (sum == num) {
       return true
-    }
-    x = x + 1
-    y = x * x
-  }
-  return false
-}
+    } 
+  }return false
+};
+
+var isPerfectSquare = function (num) {
+  let j = 1
+  let sum = 0//用一个参数记录两个数的乘积结果
+
+  for (let i = 1; sum <= num; i++, j++) {//同时加上一，遍历所有的乘积
+    sum = i * j
+    if (sum == num) {//每一次循环都作比较，是否与输入的形参相同，直至找到相等的输出true，或直至遍历结束，返回false
+      return true
+    } 
+  }return false
+};
