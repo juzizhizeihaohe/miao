a13ca492e0a4d9eed5d6979c9d4564ed29537381
xzc <xzc990310@gmail.com>
2022-03-14 11:00:04 -0400

    11day

--- 力扣/69.js
+++ 力扣/69.js
@@ -5,11 +5,17 @@
 var mySqrt = function (x) {
   let min = 0
   let max = x
+  let a = 0
+  // for (let i = 1; i < x; i++){
+  //   if (i * i == x) {
+  //     return i
+  //   }//排除整数的可能
 
-  for (let i = 1; i < x; i++){
-    if (i * i == x) {
-      return i
-    }//排除整数的可能
-    
+  // }
+  while (a * a != x) {
+    let b = a / 2
+    if (b * b > x) {
+
+    }
   }
 };
