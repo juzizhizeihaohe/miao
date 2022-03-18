78863b01a9b8c64bb77a6e7b015b0d2b8dc27e8e
xzc <xzc990310@gmail.com>
2022-03-18 09:13:00 -0400

    12day

---
+++ 力扣/167.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] == target) {
+        return [i + 1, j + 1]
+      }
+    }
+  }
+};//笨b双指针
+
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let i = 0
+  let j = numbers.length - 1
+
+  while (numbers[i] + numbers[j] != target) {
+    if (numbers[i] + numbers[j] > target) {
+      j--
+    } if (numbers[i] + numbers[j] < target) {
+      i++
+    }
+  } return [i + 1, j + 1]
+}//正经的简单双指针
+
+
+
