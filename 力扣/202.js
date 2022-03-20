6a55706da8e87d80e72188c4f137c7015dce3bc2
xzc <xzc990310@gmail.com>
2022-03-20 03:48:50 -0400

    14day

--- 力扣/202.js
+++ 力扣/202.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var isHappy = function (n) {
-  
+
   let a = []
 
   while (true) {
@@ -30,6 +30,23 @@ var isHappy = function (n) {
  * @param {number} n
  * @return {boolean}
  */
- var isHappy = function(n) {
-
+var isHappy = function (n) {
+  let a = []
+  while (true) {
+    let sum = 0
+    while (n > 0) {
+      let ng = n % 10
+      n = (n - ng) / 10
+      sum += ng * ng
+    } if (sum == 1) {
+      return true
+    } else {
+      for (let i = 0; i < a.length; i++) {
+        if (sum == a[i]) {
+          return false
+        }
+      } a.push(sum)
+      n = sum
+    }
+  }
 };
