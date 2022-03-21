ee36a9fd97b93d2d3056079078a2e149923f3f6b
xzc <xzc990310@gmail.com>
2022-03-21 09:58:11 -0400

    14day

--- 力扣/202.js
+++ 力扣/202.js
@@ -49,7 +49,7 @@ var isHappy = function (n) {
       n = sum
     }
   }
-};
+};//不知道递归的假退出条件，只能用数组记录每一个求到的sum值，用第二个循环遍历有没有第二次出现的sum值，因为这个求值结果本身就是一个循环，他回一直复现，绕圈圈，用这个方式得到退出循环的出口
 
 
 var isHappy = function (n) {
@@ -64,3 +64,4 @@ var isHappy = function (n) {
     return false
   } return isHappy(sum)
 }
+//这里就是要用递归，只要给循环真假两种方式的出口，不陷入死循环，就可以正常的运行递归，关键是要找到真假的条件
