839b92017ff8ebe10089e7da6b76a9c5dd20e0c5
xzc <xzc990310@gmail.com>
2022-03-31 10:44:23 -0400

    20day

--- 力扣/14.js
+++ 力扣/14.js
@@ -3,13 +3,5 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-  let a = strs[0][0]
-  let b = ''
 
-  for (let j = 0; j < strs.length; j++) {
-    for (let i = 0; i < strs[j].length; i++) {
-      if (strs[j][i] == strs[j++][i])
-        a = a + strs[j][i]
-    }
-  } return a
 };
