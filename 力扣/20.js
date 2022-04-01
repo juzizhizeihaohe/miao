2663a5612ac2421bacc0149f254a1d9eee828cdd
xzc <xzc990310@gmail.com>
2022-04-01 10:40:59 -0400

    20day

--- 力扣/20.js
+++ 力扣/20.js
@@ -2,6 +2,13 @@
  * @param {string} s
  * @return {boolean}
  */
- var isValid = function(s) {
-
-};
+var isValid = function (s) {
+  let a = []
+  for (let i = 0; i < s.length; i++) {
+    a.push(s[i])
+  }
+  if (a[j] == "(" || a[j] == "(" || a[j] == "(" || a[j] == "(" || a[j] == "(" || ) {
+    for (let j = 0; j < a.length; j++) {
+    }
+  };
+}
