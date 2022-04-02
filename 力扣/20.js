5c07e942318d3bf3252093687a64e972e981a268
xzc <xzc990310@gmail.com>
2022-04-02 02:31:10 -0400

    21day

--- 力扣/20.js
+++ 力扣/20.js
@@ -4,11 +4,22 @@
  */
 var isValid = function (s) {
   let a = []
+  let b = []
   for (let i = 0; i < s.length; i++) {
     a.push(s[i])
   }
-  if (a[j] == "(" || a[j] == "(" || a[j] == "(" || a[j] == "(" || a[j] == "(" || ) {
-    for (let j = 0; j < a.length; j++) {
+  for (let j = 0; j < a.length; j++) {
+    if (a[j] == "(") {
+      b.push(a.includes(")"))
+    } if (a[j] == "[") {
+      b.push(a.includes("]"))
+    } if (a[j] == "{") {
+      b.push(a.includes("}"))
     }
-  };
+  } if (b.includes(false)) {
+    return false
+  } return true
 }
+ 
+//只能判断是不是同时有两个符号存在，不能判定顺序是否正确
+
