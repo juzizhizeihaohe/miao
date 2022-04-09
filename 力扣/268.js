f088e8e39d55c4aca2e06e471a770fd6b623a2f8
xzc <xzc990310@gmail.com>
2022-04-09 10:39:05 -0400

    20day

--- 力扣/268.js
+++ 力扣/268.js
@@ -19,3 +19,31 @@ var missingNumber = function (nums) {
     return max + 1
   }
 };
+//映射
+
+//因为知道他们是有序数组，所以可以直接把他们的理论值加起来，相减，得到结果
+
+
+var missingNumber = function (nums) {
+  let max = 0
+  let sum = 0
+  let sjsum = 0
+  let b = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > max) {
+      max = nums[i]
+    }
+     for (let j = 0, c = 0; j < a.length; j++, c++) {
+    if (a[j] != c) {
+      return j
+    }
+  } if (nums.length > max) {
+    return max + 1
+  }
+  } for (let j = 0; j < max; j++) {
+    b = b + 1
+    sum = b + sum
+  } for (let p = 0; p < nums.length; p++) {
+    sjsum += nums[p]
+  } return sum - sjsum
+}
---
+++ 力扣/977.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  let a = []
+  let max = 0
+  for (let i = 0; i < nums.length; i++) {
+    a.push(nums[i] * nums[i])
+  } for (let i = 0; i < a.length; i++) {
+    if (nums[i] > max) {
+      nums[i] = max
+    }
+  }for(let i = 0)
+};
