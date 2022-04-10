762edf1c6cbf256f050a2b390fc24091d129c7be
xzc <xzc990310@gmail.com>
2022-04-10 03:36:17 -0400

    18day

--- 力扣/977.js
+++ 力扣/977.js
@@ -4,12 +4,14 @@
  */
 var sortedSquares = function (nums) {
   let a = []
+  let b = []
   let max = 0
   for (let i = 0; i < nums.length; i++) {
     a.push(nums[i] * nums[i])
   } for (let i = 0; i < a.length; i++) {
+    b[nums[i]] = nums[i]
     if (nums[i] > max) {
       nums[i] = max
     }
-  } 
+  } return b
 };
