357dad6d9b16134d10a6c750f3827f35666c306c
xzc <xzc990310@gmail.com>
2022-04-10 05:29:57 -0400

    18da

--- 力扣/415.js
+++ 力扣/415.js
@@ -14,3 +14,28 @@ var addStrings = function (num1, num2) {
     } else if (Number(num1[i]) + Number(num2[j]) < 10) { a[i] = Number(num1[i]) + Number(num2[j]) }
   } return a
 };
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function (num1, num2) {
+  let long1 = num1.length
+  let long2 = num2.length
+
+  let r = ''
+  let c = 0
+
+  for (let i = long1 - 1, j = long2 - 1; i >= 0 || j >= 0; i--, j--) {
+    let a = Number(num1[i] ?? 0)
+    let b = Number(num2[j] ?? 0)
+    let s = a + b + c
+    r = s % 10 + r
+    c = s > 9 ? 1 : 0
+  } if (c) {
+    return c + r
+  } else {
+    return r
+  }
+};
