b9580ffcdae14bb4df74720eda70b5a9fbebd9b0
xzc <xzc990310@gmail.com>
2022-04-14 07:28:36 -0400

    20day

--- 力扣/387.js
+++ 力扣/387.js
@@ -1,3 +1,12 @@
-var firstUniqChar = function(s) {
+var firstUniqChar = function (s) {
+  for (let i = 0; i < s.length; i++) {
+    let a = s[i]
+    for (let j = 0; j < s.length; j++) {
+      if (a == s[i + 1]) {
+        a = s[a++]
+      }
+    }
+
+  } return i
 
 };
