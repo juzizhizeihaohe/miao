8e46f131b5fe143aeaa2e60733b27426bd4b23d8
xzc <xzc990310@gmail.com>
2022-04-16 02:37:20 -0400

    21day

--- 力扣/387.js
+++ 力扣/387.js
@@ -1,23 +1,38 @@
 var firstUniqChar = function (s) {
-  let a = []
-  let q = 0
+  let a = {}
 
-  for (let b of s) {
-    a.push(b)
+  for (let i = 0; i < s.length; i++) {
+    let b = s[i]
+    if (b in a) {
+      a[b]++
+    } else {
+      a[b] = 1
+    }
   }
-  let f = Array(a.length).fill(0)
-  for (let c = 0; c < a.length; c++) {
-    let d = a[c]
+  for (let j = 0; j < s.length; j++) {
+    b = s[j]
+    if (a[b] == 1) {
+      return j
+    }
+  } return -1
+};
 
-    for (let e = c + 1; e < a.length; e++) {
-      if (d == a[e]) {
-        f[c]++
-      }
-    } for (let s of f) {
-      if (s == 0) {
-        return q
-      }
-      q++
-    }  
-  }
+var firstUniqChar = function (s) {
+  let a = {}//用一个空数组，放置s中的每一项
+
+  for (let i = 0; i < s.length; i++) {
+    let b = s[i]//记录s遍历的项
+    if (b in a) {//如果其已经被记录在a对象中，就给这项加上1
+      a[b]++
+    } else {
+      a[b] = 1//如果没有被记录其中，这一项就等于1
+    }
+  }//循环结束记录完成
+
+  for (let j = 0; j < s.length; j++) {
+    let w = s[j]//记录s遍历的项
+    if (a[w] == 1) {//如果a数组中记录的值有赋值为1的项，就直接输出他的下标
+      return j
+    }
+  } return -1//如果没有下标值为1的，如题所示，输出-1
 };
