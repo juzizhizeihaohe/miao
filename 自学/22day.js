94c6b015c7ba814871ae764cb47b068bc11da1f4
xzc <xzc990310@gmail.com>
2022-04-19 09:56:34 -0400

    22day

--- 自学/22day.js
+++ 自学/22day.js
@@ -11,3 +11,19 @@ function arrayTolList(array) {
     nodes[b].next = nodes[i + 1]
   } return nodes[0]
 }
+
+var reverseList = function (head) {
+  if (head == null) {
+    return null
+  }
+  var nodes = []
+  while (head) {
+    nodes.push(head)
+    head = head.next
+  }
+  nodes.reverse()
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].next = nodes[i + 1] ?? null
+  }
+  return nodes[0]
+}
