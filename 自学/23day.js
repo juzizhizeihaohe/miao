0e1bf211bc5066601c166ebfe86c04b9c2d773a9
xzc <xzc990310@gmail.com>
2022-04-22 10:45:18 -0400

    26day

---
+++ 自学/23day.js
@@ -0,0 +1,37 @@
+
+function arrayToList(array) {
+  let mubiao = []
+  for (let i = 0; i < array.length; i++) {
+    let nod = {
+      val: array[i],
+      next: NaN
+    }
+    mubiao.push(nod)
+  }
+  for (let j = 0; j < mubiao.length; j++) {
+    mubiao[j].next = [j + 1]
+  } console.log(mubiao)
+}
+
+function arrayToList2(array) {
+  let mubiao = []
+  let j = 0
+  for (let i = 0; i < array.length; i++) {
+    let nod = {
+      val: array[i],
+      next: j++
+    }
+    mubiao.push[nod]
+  } return mubiao
+}
+
+function arrayToArray(head) {
+  let mubiao = []
+  for (let i = 0; i < head.length; i++) {
+    mubiao.push(head[i].val)
+  }
+}
+
+function sow(asdf) {
+  
+}
