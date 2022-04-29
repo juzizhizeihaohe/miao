43ed4e9534c68de8317e0ee6850e158559a4d3d3
xzc <xzc990310@gmail.com>
2022-04-29 10:24:49 -0400

    27day

---
+++ 自学/27day.js
@@ -0,0 +1,7 @@
+//slice中文翻译是切、割的意思，在JavaScript中是一个切分方法。slice() 方法可以提取字符串中我们需要的某个部分。返回返回一个新的数组，以一个提取的新的字符串的形式出现。
+let a = [a, b, c, d, e, f]
+let b = a.slice(a, 4)
+b = [a, b, c, d]
+let c = a.slice(4)
+c = [e, f]
+ 
