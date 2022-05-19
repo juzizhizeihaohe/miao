21020653d001c7674318ee61f92de4377ba872c9
xzc <xzc990310@gmail.com>
2022-05-19 02:00:50 -0400

    lodash

--- lodash/juzizhizeihaohe-lodash.js
+++ lodash/juzizhizeihaohe-lodash.js
@@ -2,21 +2,27 @@ var juzizhizeihaohe = {
 
 
   chunk: function (array, size) {
-    let b = []
-    let a = []
-    for (let i = 0; i < array.length; i = i + size) {
-      for (let j = i; j < i + size; j++) {
-        a.push(array[j])
-        array.shift()
-      } b.push(a)
-      if (array.length < size) {
-        a = array
-      }
-      b.push(a)
-      a = []
-    } return (b)
+    let a = array.length//原始数组长度
+    let c = a % size//取出所有完整size后的剩余长度
+    let b = parseInt(a / size)//数组里能取出几个完整的size
+    let d = []
+    let e = []
+
+    for (let i = 0, j = 0; j < b;) {
+      d.push(array[i])
+      if (d.length == size) {
+        e.push(d)
+        d = []
+        j++
+      } i++
+    }
+
+    if (c != 0) {
+      for (let k = a - c; k < a; k++) {
+        d.push(array[k])
+      } e.push(d)
+    }
+    return e
   }
 
-
- 
 }
