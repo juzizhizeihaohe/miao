7452014ddc793c9ef42759b338a7432ad3f5d739
xzc <xzc990310@gmail.com>
2022-05-18 23:51:04 -0400

    lodash

--- lodash/juzizhizeihaohe-lodash.js
+++ lodash/juzizhizeihaohe-lodash.js
@@ -17,45 +17,7 @@ var juzizhizeihaohe = {
     } console.log(b)
   },
 
-  compact: function (array) {
-    let res = []
-    let len = array.length
-    for (let i = 0; i < len; i++) {
-      if (array[i]) {
-        res.push(array[i])
-      }
-    }
-    return res
-  },
-
-  drop: function (array, n = 1) {
-    let res = []
-    let len = array.length
-    for (let i = n; i < len; i++) {
-      res.push(array[i])
-    }
-    return res
-  },
-
-  dropright: function (array, n = 1) {
-    let res = []
-    let len = array.length
-    for (let i = 0; i < len - n; i++) {
-      res.push(array[i])
-    }
-    return res
-  },
-
-  fill: function (array, filler, start = 0, end = array.length) {
-    for (let i = start; i < end; i++) {
-      array[i] = filler
-    }
-    return array
-  }
+ 
 }
 
-for (var i = 0; i < 5; i++) {
-  setTimeout(function () {
-    console.log(i++);
-  }, 4000);
-} console.log(i)
+ 
